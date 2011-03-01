09f3e55f99687d44e236609857dc6e7d5192bd85
Marcos Gabarda <marcosgabarda@gmail.com>
2011-03-01 17:39:16 +0100

    moved authoritation functions to users helper

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,32 +6,6 @@ class RoomsController < ApplicationController
 =begin
     
 =end
-    def check_signed_in
-        respond_to do |format|
-            format.html do
-                if !signed_in?
-                    flash[:notice] = 'Authentication required'
-                    redirect_to signin_path
-                end
-            end
-            format.xml { check_http_basic }
-            format.json { check_http_basic }
-        end
-    end
-    
-    def check_http_basic
-        authenticate_or_request_with_http_basic('Authentication') do |username, password|
-            user = User.authenticate(username, password)
-            if !user.nil?
-                sign_in user
-            end
-            !user.nil?
-        end
-    end
-    
-=begin
-    
-=end
     def check_owner
         room = Room.find(params[:id])
         if room.user != current_user
