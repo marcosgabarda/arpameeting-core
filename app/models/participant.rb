24af0f37a294e87052518ee7526198b83bd7f9cc
Marcos Gabarda <marcosgabarda@gmail.com>
2011-03-11 12:58:59 +0100

    fixed minor bug with prices

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -6,6 +6,7 @@ class Participant < ActiveRecord::Base
     
     def price 
         rate = 0.0
+        #return rate if room.phonebrowser_service.nil?
         # Phone price
         if phone =~ /^\+346[0-9]+/ 
             rate = room.phonebrowser_service.rate_mobile
@@ -15,7 +16,10 @@ class Participant < ActiveRecord::Base
         end
         # TODO Parse international code and apply the corresponding 
         # price.
-        duration = pb_call_finished - pb_call_started
+        duration = 0
+        if !pb_call_finished.nil? and !pb_call_started.nil?
+            duration = pb_call_finished - pb_call_started
+        end
         return (rate/60) * duration 
     end
     
